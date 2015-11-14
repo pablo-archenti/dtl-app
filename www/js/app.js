9975db0158117eb6cffa425e23ea860f4f6edba4
Pablo Archenti <pabloarchenti@gmail.com>
2015-11-14 15:49:15 -0300

    Move app files to js folder Upgrade ionic io client

---
+++ www/js/app.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app', [
+            'ionic',
+            'ionic.service.core',
+            'ngMockE2E',
+            'projects',
+            'myaccount'
+        ])
+        .run(ionicRun);
+
+    ionicRun.$inject = ['$ionicPlatform'];
+    httpMock.$inject = ['$rootScope', '$ionicPlatform', '$httpBackend', '$http'];
+
+    function ionicRun($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    }
+
+})();
