cbbabcd44b99db443b04c08974e73e934690d3e4
Pablo Archenti <pabloarchenti@gmail.com>
2016-02-29 23:03:57 -0300

    * Complete loader ui unit test

--- tests/unit/lib/ui.module.js
+++ tests/unit/lib/ui.module.js
@@ -7,32 +7,48 @@ describe('ui.module', function () {
             uiResource;
 
         beforeEach(module('ui'));
+        beforeEach(function() {
+            module({
+                '$ionicLoading': {
+                    show: sinon.spy(),
+                    hide: sinon.spy()
+                },
+                'uiResource': {
+                    getText: sinon.stub()
+                }
+            });
+        });
         beforeEach(inject(function (_loader_, _$ionicLoading_, _uiResource_) {
             loader = _loader_;
             $ionicLoading = _$ionicLoading_;
             uiResource = _uiResource_;
         }));
-        beforeEach(function() {
-            getTextStub = sinon.stub(uiResource, "getText");
-            showSpy = sinon.spy($ionicLoading, "show");
-        });
-        afterEach(function() {
-            uiResource.getText.restore();
-            $ionicLoading.show.restore();
-        });
 
         describe('show', function() {
             var key = 'key1';
 
-            it('should loading spinner with text', function() {
-                getTextStub.withArgs(key).returns('text');
-
+            it('should load spinner with text', function() {
+                uiResource.getText.withArgs(key).returns('text1');
                 loader.show(key);
-                showSpy.getCall(0).args[0].should.deep.equal({
-                    template: '<ion-spinner></ion-spinner><div>text</div>'
+                $ionicLoading.show.getCall(0).args[0].should.deep.equal({
+                    template: '<ion-spinner></ion-spinner><div>text1</div>'
                 });
             });
+            it('should load spinner without text', function() {
+                uiResource.getText.withArgs(key).returns(null);
+                loader.show(key);
+                $ionicLoading.show.getCall(0).args[0].should.deep.equal({
+                    template: '<ion-spinner></ion-spinner>'
+                });
+            });
+        });
 
+        describe('hide', function() {
+
+            it('should call ionic loader hide() method', function() {
+                loader.hide();
+                $ionicLoading.hide.calledOnce.should.equal(true);
+            });
         });
     });
 
