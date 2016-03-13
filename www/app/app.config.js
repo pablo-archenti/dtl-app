ac9a00600116ad5f4b1103d23cfacbec7b926c78
Pablo Archenti <pabloarchenti@gmail.com>
2016-03-12 23:01:48 -0300

    * Move app run call to config

--- www/app/app.config.js
+++ www/app/app.config.js
@@ -3,7 +3,7 @@
 
     var config = {
         app: {
-            debugEnabled: true
+            debug: true
         },
         dtl: {
             //apiUrlBase: 'http://api-desdetulugar.rhcloud.com/api',
@@ -39,14 +39,40 @@
     .constant('shareProject', config.shareProject)
     .config(app)
     .config(dtl)
-    .config(uiModule);
+    .config(uiModule)
+    .run(ionicRun);
 
+    ionicRun.$inject = ['$ionicPlatform', 'appConfig'];
     app.$inject       = ['$logProvider', 'appConfig'];
     dtl.$inject       = ['dtlResourceProvider', 'dtlConfig'];
     uiModule.$inject  = ['uiResourceProvider', 'appTexts'];
 
+    function ionicRun($ionicPlatform, appConfig) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+        $ionicPlatform.ready(function() {
+            var push = new Ionic.Push({
+                "debug": appConfig.debug
+            });
+
+            push.register(function(token) {
+                alert(token.token);
+                console.log("Device token:", token.token);
+            });
+        });
+    }
+
     function app($logProvider, appConfig) {
-        $logProvider.debugEnabled(appConfig.debugEnabled);
+        $logProvider.debugEnabled(appConfig.debug);
     }
 
     function dtl(dtlResourceProvider, dtlConfig) {
