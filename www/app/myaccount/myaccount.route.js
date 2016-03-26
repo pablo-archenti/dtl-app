73f0f6b09ba3a2daf43eeff2147d8ddf9d9f17ad
Pablo Archenti <pabloarchenti@gmail.com>
2016-03-26 03:08:14 -0300

    * Refactor

--- www/app/myaccount/myaccount.route.js
+++ www/app/myaccount/myaccount.route.js
@@ -10,38 +10,14 @@
     function routes($stateProvider) {
         $stateProvider
 
-        .state('app.myaccount', {
-            url: '/myaccount',
+        .state('app.login', {
+            url: '/login',
             views: {
                 menuContent: {
-                    templateUrl: 'app/myaccount/templates/myaccount.html',
-                    controller: 'MyAccountCtrl'
+                    templateUrl: 'app/myaccount/templates/login.html',
+                    controller: 'LoginCtrl'
                 }
             }
-        })
-
-        .state('app.editMyAccount', {
-            cache: false,
-            url: '/editMyAccount',
-            abstract: true,
-            views: {
-                menuContent: {
-                    templateUrl: 'app/myaccount/templates/myData.html',
-                    controller: 'EditMyAccountCtrl'
-                }
-            }
-        })
-        .state('app.editMyAccount.step1', {
-            url: '/step1',
-            templateUrl: 'app/myaccount/templates/myDataStep1.html'
-        })
-        .state('app.editMyAccount.step2', {
-            url: '/step2',
-            templateUrl: 'app/myaccount/templates/myDataStep2.html'
-        })
-        .state('app.editMyAccount.step3', {
-            url: '/step3',
-            templateUrl: 'app/myaccount/templates/myDataStep3.html'
         })
 
         .state('app.signup', {
@@ -67,14 +43,37 @@
             templateUrl: 'app/myaccount/templates/myDataStep3.html'
         })
 
-        .state('app.login', {
-            url: '/login',
+        .state('app.myaccount', {
+            url: '/myaccount',
             views: {
                 menuContent: {
-                    templateUrl: 'app/myaccount/templates/login.html',
-                    controller: 'LoginCtrl'
+                    templateUrl: 'app/myaccount/templates/myaccount.html',
+                    controller: 'MyAccountCtrl'
                 }
             }
+        })
+        .state('app.editMyAccount', {
+            cache: false,
+            url: '/editMyAccount',
+            abstract: true,
+            views: {
+                menuContent: {
+                    templateUrl: 'app/myaccount/templates/myData.html',
+                    controller: 'EditMyAccountCtrl'
+                }
+            }
+        })
+        .state('app.editMyAccount.step1', {
+            url: '/step1',
+            templateUrl: 'app/myaccount/templates/myDataStep1.html'
+        })
+        .state('app.editMyAccount.step2', {
+            url: '/step2',
+            templateUrl: 'app/myaccount/templates/myDataStep2.html'
+        })
+        .state('app.editMyAccount.step3', {
+            url: '/step3',
+            templateUrl: 'app/myaccount/templates/myDataStep3.html'
         });
     }
 })();
