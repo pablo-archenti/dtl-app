4089aeff04dd93496f6f5af5222e302bbbfc7739
Pablo Archenti <pabloarchenti@gmail.com>
2016-04-10 22:57:12 -0300

    * Protractor env set for e2e tests

---
+++ tests/protractor-e2e.js
@@ -0,0 +1,36 @@
+exports.config = {
+    allScriptsTimeout: 99999,
+
+    // The address of a running selenium server.
+    seleniumAddress: 'http://selenium:4444/wd/hub',
+
+    // The location of the selenium standalone server .jar file, relative
+    // to the location of this config. If no other method of starting selenium
+    // is found, this will default to
+    // node_modules/protractor/selenium/selenium-server...
+    //seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar',
+
+    // The port to start the selenium server on, or null if the server should
+    // find its own unused port.
+    //seleniumPort: 4444,
+
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    baseUrl: 'http://app:8101/',
+
+    framework: 'mocha',
+
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: ['functional/*.js'],
+
+    // Options to be passed to mocha.
+    mochaOpts: {
+        reporter: "spec",
+        slow: 3000,
+        timeout: 5000
+    }
+};
